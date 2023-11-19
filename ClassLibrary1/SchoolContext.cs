eddc62fd53ecb7b77bcb6efe09af2638b0bf9e52
Timileyin Ayegbayo <tayegbayo@axxess.com>
2023-11-19 17:39:56 +0100

    Add project files.

---
+++ ClassLibrary1/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolApp.Data.UserManagement;
+using System;
+
+namespace ClassLibrary1
+{
+    public class SchoolAppContext : IdentityDbContext<User>
+    {
+        public SchoolAppContext(DbContextOptions<SchoolAppContext> options)
+        {
+
+        }
+    }
+}
